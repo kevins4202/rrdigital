1cecc5fb066bbae016cec726d4331025dd86d77e
Kevin Song <song_zj@hotmail.com>
2024-11-29 20:06:57 -0500

    attempt deploy vercel

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 app.use(cors());
 app.use(express.json());
